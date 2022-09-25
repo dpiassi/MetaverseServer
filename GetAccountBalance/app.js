9bab6a380d59b78288530dba317a11e262a5e897
Luis Felipe <luis.felipe@koode.io>
2022-09-25 18:20:24 -0300

    Account Balance Completed

--- GetAccountBalance/app.js
+++ GetAccountBalance/app.js
@@ -1,28 +1,88 @@
 const axios = require('axios')
-let url = 'https://challenge.hackathonbtg.com/customers/v1/personal/qualifications';
-let config = {
-  headers:{
-    'organizationid': '69665991-da55-4aac-a1f2-32d23daba8fe',
-    'customerid': '595.080.896-84'
-  }
-};
-
-let response;
-let data;
-let status;
+let data
+let status
 
 exports.lambdaHandler = async (event, context) => {
-    await axios.get(url, config).then(res => {
-        status = res.status
-        data = res.data
-    }).catch(error => {
-        console.log(error)
-        status = 500;
-        data = "";
-    })
+    const customerId = event.queryStringParameters.customerId;
 
-    return response = {
-      'statusCode': status,
-      'body': data.data
+    const accountBalances = await getAccountsBalance(customerId)
+
+    return {
+        'totalBalanceBetweenAccounts': accountBalances
     }
 };
+
+async function getOrgId(userID){
+    let jsonData
+    let organizationIds = new Array()
+    let config = {
+        method: 'get',
+        url: 'https://squad-12-lepoli.s3.sa-east-1.amazonaws.com/mock-cliente-com-sua-instituicao-financeira.json',
+        headers: { }
+    };
+
+    await axios(config)
+        .then(function (response) {
+            jsonData = response.data
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    for (var index = 0; index < jsonData.length; index++){
+        if(jsonData[index].customerId == userID){
+            organizationIds.push(jsonData[index].organizationId)
+        }
+    }
+
+    return organizationIds
+}
+
+async function getAccountId(organizationId, customerId){
+    const accountURL = 'https://challenge.hackathonbtg.com/accounts/v1/accounts'
+    const config = {
+        headers:{
+            'organizationid': organizationId,
+            'customerid': customerId
+        }
+    };
+    let accountData
+
+    await axios.get(accountURL, config).then(res => {
+        accountData = res.data.data
+    }).catch(error => {
+        console.log(error)
+        accountData = ''
+    })
+
+    return accountData[0].accountId
+}
+
+async function getAccountsBalance(customerId){
+
+    const orgIds = await getOrgId(customerId)
+
+    let accountBalances = 0
+    for (var i = 0; i < orgIds.length; i++) {
+        const url = 'https://challenge.hackathonbtg.com/customers/v1/personal/qualifications'
+        let config = {
+            headers:{
+                'organizationid': orgIds[i],
+                'customerid': customerId
+            }
+        };
+
+        let otherAccounts
+        await axios.get(url, config).then(res => {
+            otherAccounts = res.data.data
+        }).catch(error => {
+                console.log(error)
+                otherAccounts = "";
+            }
+        )
+        const patrimony = parseFloat(otherAccounts.informedPatrimony.amount)
+        accountBalances += patrimony
+    }
+
+    return accountBalances
+}
